0eec2662f538d20477eb4c1943f464c06ee18d99
Jonathan Samson <samsonjj@vcu.edu>
2019-04-10 06:26:11 +0000

    second commit

--- qa-system.pl
+++ qa-system.pl
@@ -5,40 +5,89 @@ use feature 'say';
 use WWW::Wikipedia;
 
 my $wiki = WWW::Wikipedia->new( clean_html => 1 );
- 
-# Flag to continue the main loop.
-my $run = 1;
 
 # Counts the number of main loops, i.e. the number of questions asked so far.
 my $questionCounter = 0;
 
+# Print intro line.
+say "*** This is a QA system by Jonathan Samson. It will try to answer questions that start with Who, What, When or Where. Enter \"exit\" to leave the program.";
+
 # Main loop. Get query, expand query, search for queries, pick answer, parse answer, return answer.
-while( $run ) {
+while( 1 ) {
 
-    say "*** This is a QA system by Jonathan Samson. It will try to answer questions that start with Who, What, When or Where. Enter \"exit\" to leave the program.";
+    print "QUESTION: ";
 
+    # Read query from standard input.
     my $query = <STDIN>;
+    $questionCounter++;
 
     # Remove endline character.
     chomp $query;
-    lc $query;
+    $query = lc $query;
 
     # Remove trailing whitespace
     $query =~ s/^\s+|\s+$//g;
 
+    # If the query equals "exit", quit the program.
+    if( $query eq "exit" ) {
+        return;
+    }
+
     # If the question does not begin with who, what, when, or where, return a default response.
     if( $query !~ /^(who|what|when|where)/ ) {
         say "Please ask a question that begins with who, what, when or where.";
+        next;
     }
 
-    say "Count: $questionCounter, $query";
+    # Store the query expansions here. They should be ordered by most precise to least precise.
+    my @queryExpansion = ();
+    # Store the key term here, which we will search for in wikipedia.
+    my $keyTerm;
 
+    # Run a bunch of regex matches on the query. If matches, create query expansion.
 
+    #TODO
+    say "QUERY: |$query|";
 
-    $questionCounter++;
-    if( $query eq "exit" ) {
-        $run = 0;
+    # if( $query =~ /^who is\s+(.*)\s+\?$/ ) {
+    if( $query =~ /^who is\s+(.*)\s*\?$/ ) {
+        $keyTerm = $1;
+        push @queryExpansion, qr/$keyTerm is ([^.])*\./i;
+        push @queryExpansion, qr/$keyTerm.*was ([^.]*)\./i;
+        push @queryExpansion, qr/(George Washington)/i;
     }
+    else {
+        say "I am sorry I don't know the answer.";
+        next;
+    }
+
+    # TODO
+    say "KEYTERM: |$keyTerm|";
+
+    # Get Wikipedia document.
+    my $result = $wiki->search( $keyTerm );
+    # Get text from document.
+    my $wikiText = $result->text();
+    # Remove newline characters from wikitext, since WWW::Wikipedia adds extras for aesthetics.
+    $wikiText =~ s/\R/ /g;
+
+    #TODO
+    say "TEXT: |$wikiText|";
+
+    # Stores the captured text, which matched the expansion.
+    my $capture = "";
+
+    # Search for each expansion. Return the first match.
+    for my $expansion (@queryExpansion) {
+        #TODO
+        say "EXPANSION: |$expansion|";
+        if( $wikiText =~ /$expansion/ ) {
+            $capture = $1;
+            last;
+        }
+    }
+
+    say "ANSWER: $capture";
 }
 
 ## search for 'perl' 
